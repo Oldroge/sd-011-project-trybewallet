0b2d5febb1e6b588300aa609f46e1825cfe4107c
OldRoge <rogeriomrjr@icloud.com>
2022-03-13 20:14:25 -0300

    feat: add a state to login

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,18 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const wasLogged = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case LOGIN:
+    return {
+      email: action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wasLogged;
